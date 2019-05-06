a7d28e335459dfdf2ed6bec6f84c113e467aca93
Neil Manvar <neil@sentry.io>
2019-05-05 22:42:56 -0700

    model binding working. need to add sentry tags/instrumentation back in

--- deploy.ps1
+++ deploy.ps1
@@ -3,13 +3,13 @@ $sentry_project="dotnet-demo"
 $version=sentry-cli releases propose-version
 
 #Create Release
-sentry-cli releases -o $sentry_org new -p $sentry_project $version
+# sentry-cli releases -o $sentry_org new -p $sentry_project $version
 
 #Associate Commits
-sentry-cli releases -o $sentry_org -p $sentry_project set-commits --auto $version
+# sentry-cli releases -o $sentry_org -p $sentry_project set-commits --auto $version
 
 #Set new value for release version environment variable
 $Env:SENTRY_RELEASE=$version
 
-#Launch app 
+#Launch app
 dotnet run
