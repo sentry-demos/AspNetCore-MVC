b4e544337af92e33802cc3233f7fc4dab9aa69c0
Dustin Bailey <bailey.dust@gmail.com>
2021-01-10 15:50:50 -0800

    FIXES Dependency error for IHostBuilder

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Sentry;
