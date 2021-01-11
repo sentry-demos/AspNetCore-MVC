a3d6048004414221155384cd3f503726794760fe
Dustin Bailey <bailey.dust@gmail.com>
2021-01-10 16:05:41 -0800

    AddServices Syntax error

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace AspNetCoreMVC
             });
 
             // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.addControllers();
+            services.AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
