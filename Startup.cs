f67a870df2b283194099f0fb57ac1b4adcf5da95
Dustin Bailey <bailey.dust@gmail.com>
2021-01-10 16:17:28 -0800

    testing suspect commits

--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,7 @@ namespace AspNetCoreMVC
                 });
             });
 
-            // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+           
             services.AddControllers();
         }
 
